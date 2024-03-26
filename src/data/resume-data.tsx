fc3956f75ffdd8e7007ac1e4fb7112c569de50af
Samuel Gopeh <sammygopeh@gmail.com>
2024-03-26 08:41:35 +0100

    added professional experience

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -3,7 +3,7 @@ import {
   BlendingBytesLogo,
   HeyfoodLogo,
   SatgLogo,
-  ZuriLogo
+  ZuriLogo,
 } from "@/images/logos";
 
 export const RESUME_DATA = {
@@ -19,7 +19,8 @@ export const RESUME_DATA = {
   //   "As a full stack developer with five years of hands-on experience, I excel in the dynamic realm of client-side and server-side development. Driven by a profound passion for my craft, I remain dedicated to creating scalable and efficient solutions while maintaining clean architecture. I stay at the forefront of emerging technologies by ensuring my skills are consistently honed and adapted.",
   summary:
     "As a backend developer with five years of hands-on experience, I excel in the dynamic realm of client-side development. Driven by a profound passion for my craft, I remain dedicated to creating scalable and efficient solutions while maintaining clean architecture. I stay at the forefront of emerging technologies by ensuring my skills are consistently honed and adapted.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/54728958?s=400&u=d6be5c87f709017b235b8f80e4eb3be9be8b43d3&v=4",
+  avatarUrl:
+    "https://avatars.githubusercontent.com/u/54728958?s=400&u=d6be5c87f709017b235b8f80e4eb3be9be8b43d3&v=4",
   // personalWebsiteUrl: "https://gopeh.com",
   contact: {
     email: "sammygopeh@gmail.com",
@@ -34,7 +35,7 @@ export const RESUME_DATA = {
         name: "LinkedIn",
         url: "https://ng.linkedin.com/in/samuel-gopeh-3847719b",
         icon: LinkedInIcon,
-      }
+      },
     ],
   },
   education: [
@@ -60,7 +61,7 @@ export const RESUME_DATA = {
         "Improved user experience by implementing the backend for a faceted search.",
         "Reduced database cost by optimizing existing inefficient queries.",
         "Improved developer experience by providing comprehensive insights into backend code architecture.",
-      ]
+      ],
     },
     {
       company: "Heyfood",
@@ -72,15 +73,27 @@ export const RESUME_DATA = {
       end: "2023",
       description:
         "Implemented a vast array of new features including an order assignment system, a notification system, a vendor payout system, several dashboards to mention a few. Led the team, developed deployment strategies for new features, carried-out migration from Mongodb to Postgresql, and many more. Technologies: TypeScript, Javascript, Node.js, Express.js, MongoDB, Postgresql, Redis, Firebase, AWS, React.js/Next.js and multiple integrations with third party APIs.",
-        accomplishments: [
-          "Improved delivery time by 20% by automating the order assignment and batching process.",
-          "Improved vendor and driver satisfaction by implementing a flexible automatic payout system.",
-          "Drove growth via rapid iteration and implementation of features to support growth hacks.",
-          "Improved customer support response time by developing a real-time monitoring dashboard for live orders.",
-          "Reduced grocery vendor onboarding time by 50% through the implementation of an automatic inventory ingestion system.",
-          "Improved overall operations efficiency by implementing interfaces that provided insights into the system and allowed authorized employees to take immediate action when necessary.",
-          "Reduced database cost by 65% by migrating to a more suitable database (MongoDB to PostgreSQL)."
-        ]
+      accomplishments: [
+        "Improved delivery time by 20% by automating the order assignment and batching process.",
+        "Improved vendor and driver satisfaction by implementing a flexible automatic payout system.",
+        "Drove growth via rapid iteration and implementation of features to support growth hacks.",
+        "Improved customer support response time by developing a real-time monitoring dashboard for live orders.",
+        "Reduced grocery vendor onboarding time by 50% through the implementation of an automatic inventory ingestion system.",
+        "Improved overall operations efficiency by implementing interfaces that provided insights into the system and allowed authorized employees to take immediate action when necessary.",
+        "Reduced database cost by 65% by migrating to a more suitable database (MongoDB to PostgreSQL).",
+      ],
+    },
+    {
+      company: "Pledre",
+      link: "https://pledre.com/",
+      badges: ["Remote"],
+      title: "Frontend Developer (Contract)",
+      logo: HeyfoodLogo,
+      start: "2022",
+      end: "2022",
+      description:
+        "Implemented a vast array of new features including an order assignment system, a notification system, a vendor payout system, several dashboards to mention a few. Led the team, developed deployment strategies for new features, carried-out migration from Mongodb to Postgresql, and many more. Technologies: TypeScript, Javascript, Node.js, Express.js, MongoDB, Postgresql, Redis, Firebase, AWS, React.js/Next.js and multiple integrations with third party APIs.",
+      accomplishments: ["Built a fast and responsive landing page"],
     },
     // {
     //   company: "Zuri Team",
@@ -103,10 +116,10 @@ export const RESUME_DATA = {
       end: "2021",
       description:
         "Worked in a team of 3 to build a blog and an e-commerce website for the company. Technologies: Python, Django, React.js, Next.js, Javascript, PostgreSQL",
-        accomplishments: [
-          "Improved brand awareness by building a blog for the company",
-          "Assited in generating revenue by building and maintaining an e-commerce website.",
-        ]
+      accomplishments: [
+        "Improved brand awareness by building a blog for the company",
+        "Assited in generating revenue by building and maintaining an e-commerce website.",
+      ],
     },
   ],
   skills: [
@@ -134,7 +147,7 @@ export const RESUME_DATA = {
     "CSS3",
     "Git",
     "Bitbucket",
-    "Jira"
+    "Jira",
   ],
   projects: [
     {
@@ -145,9 +158,10 @@ export const RESUME_DATA = {
         "Azure Blob Storage",
         "Firebase",
         "TypeScript",
-        "Node.js"
+        "Node.js",
       ],
-      description: "An npm package that provides a simple interface for uploading files from a node.js application to major cloud storage platforms, including S3, Google Cloud Storage, Azure Blob Storage, Firebase, and Google Drive.",
+      description:
+        "An npm package that provides a simple interface for uploading files from a node.js application to major cloud storage platforms, including S3, Google Cloud Storage, Azure Blob Storage, Firebase, and Google Drive.",
       logo: SatgLogo,
       link: {
         label: "https://www.npmjs.com/package/bufferbus",
@@ -156,14 +170,9 @@ export const RESUME_DATA = {
     },
     {
       title: "Paychip",
-      techStack: [
-        "TypeScript",
-        "Node.js",
-        "Postgresql",
-        "Redis",
-        "Jest",
-      ],
-      description: "A platform for businesses to create and manage wallets for their users. It implements a RESTful API for creating, funding, and withdrawing from wallets while maintaining accurate transaction records, allowing businesses to focus on writing their business logic.",
+      techStack: ["TypeScript", "Node.js", "Postgresql", "Redis", "Jest"],
+      description:
+        "A platform for businesses to create and manage wallets for their users. It implements a RESTful API for creating, funding, and withdrawing from wallets while maintaining accurate transaction records, allowing businesses to focus on writing their business logic.",
       link: {
         href: "https://github.com/gopeeey/paychip",
       },
@@ -180,7 +189,8 @@ export const RESUME_DATA = {
         "HTML",
         "CSS",
       ],
-      description: "Short for Super Awesome Typing Game. A real-time multiplayer typing game",
+      description:
+        "Short for Super Awesome Typing Game. A real-time multiplayer typing game",
       logo: SatgLogo,
       link: {
         label: "satg.gopeh.com",
@@ -189,25 +199,18 @@ export const RESUME_DATA = {
     },
     {
       title: "U.O.D",
-      techStack: [
-        "JavaScript",
-        "Node.js",
-        "HTML",
-        "CSS",
-      ],
-      description: "Up Or Down. A simple application for monitoring the up or down status of a user's desired sites. It is built entirely in Node.js without third-party libraries (as an added challenge).",
+      techStack: ["JavaScript", "Node.js", "HTML", "CSS"],
+      description:
+        "Up Or Down. A simple application for monitoring the up or down status of a user's desired sites. It is built entirely in Node.js without third-party libraries (as an added challenge).",
       link: {
         href: "https://github.com/gopeeey/up_or_down",
       },
     },
     {
       title: "Irrational",
-      techStack: [
-        "Javascript",
-        "HTML",
-        "CSS"
-      ],
-      description: "A simple demonstration of simple and complex patterns in rational and irrational numbers.",
+      techStack: ["Javascript", "HTML", "CSS"],
+      description:
+        "A simple demonstration of simple and complex patterns in rational and irrational numbers.",
       link: {
         href: "https://irrational.gopeh.com",
       },
